92fcaadad736ca3c365127a9ccb02070f6ae6f46
Adrian Carolli <adrian.carolli1@gmail.com>
2019-04-12 19:17:33 -0400

    hook up context

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,8 +1,16 @@
 import React from "react";
 
+import LanguageContext from "../contexts/LanguageContext";
+
 class Button extends React.Component {
   render() {
-    return <button className="ui button primary">Submit</button>;
+    return (
+      <button className="ui button primary">
+        <LanguageContext.Consumer>
+          {value => (value === "english" ? "Submit" : "Voorlegan")}
+        </LanguageContext.Consumer>
+      </button>
+    );
   }
 }
 
